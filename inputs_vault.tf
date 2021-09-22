a5af0aa28f05328bab7094199351f6330fb87471
Michael Aldridge <maldridge@voiceops.com>
2021-09-22 10:47:23 -0500

    vault: update to 1.8.2

--- inputs_vault.tf
+++ inputs_vault.tf
@@ -7,7 +7,7 @@ variable "vault_server" {
 variable "vault_version" {
   type        = string
   description = "Vault server version"
-  default     = "1.8.1"
+  default     = "1.8.2"
 }
 
 variable "vault_ui" {
