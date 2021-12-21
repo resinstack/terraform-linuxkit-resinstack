52a861a7697b4c3023e559f788568206c13ea9f9
Michael Aldridge <aldridge.mac@gmail.com>
2021-12-21 01:21:23 -0600

    vault: update to 1.9.1

--- inputs_vault.tf
+++ inputs_vault.tf
@@ -7,7 +7,7 @@ variable "vault_server" {
 variable "vault_version" {
   type        = string
   description = "Vault server version"
-  default     = "1.9.0"
+  default     = "1.9.1"
 }
 
 variable "vault_ui" {
