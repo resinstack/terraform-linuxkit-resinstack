18e15fe369524af2e6f8f161145d8063c83d592f
Michael Aldridge <aldridge.mac@gmail.com>
2022-02-11 14:47:38 -0600

    vault: bump to 1.9.3

--- inputs_vault.tf
+++ inputs_vault.tf
@@ -7,7 +7,7 @@ variable "vault_server" {
 variable "vault_version" {
   type        = string
   description = "Vault server version"
-  default     = "1.9.1"
+  default     = "1.9.3"
 }
 
 variable "vault_ui" {
