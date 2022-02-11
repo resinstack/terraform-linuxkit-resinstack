8d2a978d4525e3f9fb9316650b82350ef0507d55
Michael Aldridge <aldridge.mac@gmail.com>
2022-02-11 15:09:40 -0600

    docker: bump to 20.10.12

--- inputs_docker.tf
+++ inputs_docker.tf
@@ -1,7 +1,7 @@
 variable "docker_version" {
   type        = string
   description = "Docker version to run"
-  default     = "20.10.6-dind"
+  default     = "20.10.12-dind"
 }
 
 variable "enable_docker" {
