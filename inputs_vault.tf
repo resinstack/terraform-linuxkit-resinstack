ca3e5f4e3a7a7e173a1ce307ccb74c05d236d914
Michael Aldridge <aldridge.mac@gmail.com>
2022-03-28 11:14:06 -0500

    vault: update to 1.10.0.

--- inputs_vault.tf
+++ inputs_vault.tf
@@ -7,7 +7,7 @@ variable "vault_server" {
 variable "vault_version" {
   type        = string
   description = "Vault server version"
-  default     = "1.9.3"
+  default     = "1.10.0"
 }
 
 variable "vault_ui" {
