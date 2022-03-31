f3eaaa187d9aacc0f35fa0ad127bcd541b0249ca
Michael Aldridge <aldridge.mac@gmail.com>
2022-03-30 21:45:40 -0500

    system: update various system components

--- inputs_system.tf
+++ inputs_system.tf
@@ -19,7 +19,7 @@ variable "system_version_unified" {
 variable "system_version_init" {
   type        = string
   description = "Use a different init than the unified version number"
-  default     = "7e3d51e6ab5896ecb36a4829450f7430f2878927"
+  default     = "8f1e6a0747acbbb4d7e24dc98f97faa8d1c6cec7"
 }
 
 variable "system_version_runit" {
@@ -31,13 +31,13 @@ variable "system_version_runit" {
 variable "system_version_runc" {
   type        = string
   description = "Use a different runc than the unified version number"
-  default     = "9f7aad4eb5e4360cc9ed8778a5c501cce6e21601"
+  default     = "f01b88c7033180d50ae43562d72707c6881904e4"
 }
 
 variable "system_version_containerd" {
   type        = string
   description = "Use a different containerd than the unified version number"
-  default     = "2f0907913dd54ab5186006034eb224a0da12443e"
+  default     = "de1b18eed76a266baa3092e5c154c84f595e56da"
 }
 
 variable "system_version_ca_certificates" {
@@ -49,7 +49,7 @@ variable "system_version_ca_certificates" {
 variable "system_version_sysctl" {
   type        = string
   description = "Use a different sysctl than the unified version number"
-  default     = "0dc8f792fc3a58afcebcb0fbe6b48de587265c17"
+  default     = "bdc99eeedc224439ff237990ee06e5b992c8c1ae"
 }
 
 variable "system_version_sysfs" {
@@ -61,67 +61,67 @@ variable "system_version_sysfs" {
 variable "system_version_dhcpcd" {
   type        = string
   description = "Use a different dhcpcd than the unified version number"
-  default     = ""
+  default     = "52d2c4df0311b182e99241cdc382ff726755c450"
 }
 
 variable "system_version_acpid" {
   type        = string
   description = "Use a different acpid than the unified version number"
-  default     = "e9a94e593d6be2fc1b3eb6d566f23ac9ca9807fd"
+  default     = "c05a368754f6436b326945dc16135ba547568d8d"
 }
 
 variable "system_version_metadata" {
   type        = string
   description = "Use a different metadata than the unified version number"
-  default     = ""
+  default     = "646c00ad6c0b3fc246b6af9ccfcd6b1eb6b6da8a"
 }
 
 variable "system_version_getty" {
   type        = string
   description = "Use a different getty than the unified version number"
-  default     = ""
+  default     = "76951a596aa5e0867a38e28f0b94d620e948e3e8"
 }
 
 variable "system_version_sshd" {
   type        = string
   description = "Use a different sshd than the unified version number"
-  default     = ""
+  default     = "4696ba61c3ec091328e1c14857d77e675802342f"
 }
 
 variable "system_version_rngd" {
   type        = string
   description = "Use a different rngd than the unified version number"
-  default     = ""
+  default     = "4f85d8de3f6f45973a8c88dc8fba9ec596e5495a"
 }
 
 variable "system_version_ntpd" {
   type        = string
   description = "Use a different ntpd than the unified version number"
-  default     = ""
+  default     = "d6c36ac367ed26a6eeffd8db78334d9f8041b038"
 }
 
 variable "system_version_format" {
   type        = string
   description = "Use a different format than the unified version number"
-  default     = ""
+  default     = "7efa07559dd23cb4dbebfd3ab48c50fd33625918"
 }
 
 variable "system_version_mount" {
   type        = string
   description = "Use a different mount than the unified version number"
-  default     = ""
+  default     = "422b219bb1c7051096126ac83e6dcc8b2f3f1176"
 }
 
 variable "system_version_logwrite" {
   type        = string
   description = "Use a different logwrite than the unified version number"
-  default     = "568325cf294338b37446943c2b86a8cd8dc703db"
+  default     = "4d8aa07d4a7130239fc62b09f33e3401ecf62a38"
 }
 
 variable "system_version_memlogd" {
   type        = string
   description = "Use a different memlogd than the unified version number"
-  default     = "fe4a123b619a7dfffc2ba1297dd03b4ac90e3dd7"
+  default     = "014f86dce2ea4bb2ec13e92ae5c1e854bcefec40"
 }
 
 variable "enable_persist" {
